dc7a9f201cafb5be88dc1762481f67b984e431e1
benwagle <ben.wagle@yahoo.com>
2013-03-02 11:20:22 -0500

    finally up

---
+++ Desktop/COLLEGE/Programming/Multi-Threading/Combo.java
@@ -0,0 +1,380 @@
+import javax.swing.*;//needed for JFrame and JPanel
+import java.awt.*;//needed to draw things
+import java.util.*;//needed for scanner
+import java.io.*;//needed for File
+
+public class Combo extends JFrame
+/*
+ * using the swing class
+ * read and print a world in game of life
+ */
+{
+  public Combo()
+  {
+   
+    setSize(400,400);//sets the size of the window
+    int h= getHeight();
+    int w= getWidth();
+    JPanel panel= new JPanel();//drawings are done on panels
+    panel.setLayout(new GridLayout(1,2));
+    Life life1= new Life();
+    Test test= new Test();
+    life1.setBackground(Color.blue);
+    test.setBackground(Color.yellow);
+    panel.add(life1);
+    panel.add(test);
+    setLocationRelativeTo(null);//GUI screen is not obscured
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//produces exit message
+    setContentPane(panel);//GUIs are placed on contentPane
+    setVisible(true);//enables us to view window
+    Temp one = new Temp(life1);
+    Temp1 two = new Temp1(test, h, w,10);
+    two.start();
+    one.start();
+    
+  }
+  
+  public static void main(String[] asd)
+  {
+    new Combo();
+    
+  }
+}
+
+class Temp extends Thread
+{
+  Life anim;
+  
+  public Temp(Life anim)
+  {
+    this.anim=anim;
+  }
+  
+  public void run()
+  {
+    anim.begin();
+  }
+}
+
+class Temp1 extends Thread
+{
+  Test anim1;
+  int h,w,t;
+  
+  public Temp1(Test anim, int h,int w, int t)
+  {
+    this.anim1=anim; 
+    this.h= h;
+    this.w=w;
+    this.t=t;
+  }
+  
+  public void run()
+  {
+    anim1.move(h,w,t);
+    
+  }
+}
+
+class Life extends JPanel
+{
+  
+  final static int NROW=25, NCOL=47;
+  final static char DOT= '.';
+  char[][] grid= new char[NROW+2][NCOL+2];
+  char[][] birth= new char[NROW+2][NCOL+2];
+  boolean SameFlag = false;
+  boolean BlankFlag = false;
+  
+  public void init()
+  {
+    for(int row=0; row<=NROW+1; row++)
+    {
+      for(int col=0; col<=NCOL+1; col++)
+      {
+        birth[row][col]=DOT;
+      }
+    }
+  }
+  
+  public int neighbors(int r, int c)
+    /*checks the current world for neighbors at each point
+     * so that birth/death can be determined
+     */
+  {
+    int count=0;
+    for(int row=r-1; row<=r+1; row++)
+    {
+      for(int col=c-1; col<=c+1; col++)
+      {
+        if(grid[row][col]== 'X')
+        {
+          count++;
+        }
+      }
+    }
+      if(grid[r][c]=='X')
+      {
+        count= count-1;
+      }
+    return count;
+    }
+  
+  public void print(Graphics g)
+  {
+    int x,y;
+    y=20;
+    for(int row=1; row<=NROW; row++)
+    {
+      x=20;
+      for(int col=1; col<=NCOL; col++)
+      {
+        g.drawString(""+ birth[row][col],x,y);
+        x= x+7;
+      }
+      y= y+15;
+    }
+  }
+  
+  public void pause()
+  {
+    try
+    {
+      Thread.sleep(1500);
+    }catch(Exception ex){}
+  }
+
+  
+    public void read()
+ {
+     Scanner kbd= null;
+    try
+    {
+        kbd= new Scanner(new File("life4.dat"));
+    }catch(Exception ex) {};
+    
+    for(int row=1; row<=NROW; row++)
+    {
+      String s= kbd.next();
+      for(int col=1; col<=NCOL; col++)
+      {
+        birth[row][col]= s.charAt(col-1);
+      }
+    }
+  }
+ 
+ 
+  
+  public void swap()
+     {
+      for(int row=1; row<=NROW; row++)
+    {
+      for(int col=1; col<=NCOL; col++)
+       {
+        grid[row][col]=birth[row][col];
+        //overwrites old world with new world
+       }
+     }
+  }
+ 
+  
+  public boolean isSame()
+  {
+    for(int row=1; row<=NROW; row++)
+    {
+      for(int col=1; col<=NCOL; col++)
+      {
+        if(grid[row][col]!=birth[row][col])
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  public boolean isEmpty()
+  {
+    for(int row=1; row<=NROW; row++)
+    {
+      for(int col=1; col<=NCOL; col++)
+      {
+        if(birth[row][col]=='X')
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  public void life()
+  {
+    for(int row=1; row<=NROW; row++)
+    {
+      for(int col=1; col<=NCOL; col++)
+      {
+      if(grid[row][col]=='X')
+       {
+         if(neighbors(row,col)==2 || neighbors(row,col)==3)
+           //checks if x should be reborn
+         {
+           birth[row][col]='X';
+         }
+         if(neighbors(row,col)!=2 && neighbors(row,col)!=3)
+           //checks if x should die
+         {
+           birth[row][col]=DOT;
+         }
+       }
+       if(grid[row][col]==DOT)
+       {
+         if(neighbors(row,col)==3)
+           //checks if x should be born
+         {
+           birth[row][col]='X';
+         }
+         if(neighbors(row,col)!=3)
+           //checks if dot stays dead
+         {
+           birth[row][col]=DOT;
+         }
+       }
+      }
+    }
+  }
+  
+  public void begin() 
+  {
+    init();
+    repaint();
+    pause();
+    read();
+    repaint();
+    do
+    {
+    swap();
+    pause();
+    life();
+    repaint();
+    }while(!isSame() && !isEmpty());
+     
+    if(isSame())
+    {
+      SameFlag = true;
+      repaint();
+    }
+    
+    if(isEmpty())
+    {
+      BlankFlag = true;
+      repaint();
+    }
+  }
+  
+
+  public void paintComponent(Graphics g)//initially called by JVM
+  {
+    super.paintComponent(g);//erases previous window
+    g.setColor(Color.red);
+    print(g);
+    if(SameFlag)
+        {
+          g.drawString("The worlds are repeating!",10,450);
+        }
+       if(BlankFlag)
+        {
+          g.drawString("The world is blank!",10,450);
+        }
+  }
+
+}
+
+class Test extends JPanel
+{
+  int x=0;
+  int y=200;
+  
+  public void move(int h, int w, int t)
+  {
+    while(true)
+    {
+      
+      
+      if(x==0)
+      {
+        while(y<h)
+        {
+          x++;
+          y++;
+          repaint();//calls paintComponent
+          try
+          {
+            Thread.sleep(t);//100 milliseconds
+          }catch(Exception ex){};
+        }
+      }
+        
+       
+      
+      if(y==h)
+        {
+          while(y>0 && x<w)
+          {
+            x++;
+            y--;
+            repaint();
+            try
+            {
+              Thread.sleep(t);//100 milliseconds
+            }catch(Exception ex){};
+          }
+        }
+      
+      
+      
+      if(x==w)
+      {
+        while(y>0 && x>0)
+        {
+          x--;
+          y--;
+          repaint();
+          try
+          {
+            Thread.sleep(t);//100 milliseconds
+          }catch(Exception ex){};
+        }
+      }
+      
+      
+      if(y==0)
+      {
+        while(y<h && x>0)
+        {
+          x--;
+          y++;
+          repaint();//calls paintComponent
+          try
+          {
+            Thread.sleep(t);//100 milliseconds
+          }catch(Exception ex){};
+        }
+      }
+    
+    }
+  }
+          
+     
+public void paintComponent(Graphics g)
+  {
+    int k= 10;
+    g.setColor(Color.red);
+    super.paintComponent(g);//erases previous window
+    g.setColor(Color.red);
+    g.fillRect(x,y,k,k);
+    g.setColor(Color.black);
+    g.drawLine(0,409, 409, 409);
+    g.drawLine(409,0,409,409);
+  }
+}
